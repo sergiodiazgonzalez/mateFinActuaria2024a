f3d4d671656b1a3b3b559d3f07bf7ffaded3fb91
sergiodiazgonzalez <2350461b@umich.mx>
2024-09-19 12:48:54 -0600

    Add files via upload

---
+++ FormulasInteresSimple1.R
@@ -0,0 +1,11 @@
+# Se presentan las funciones de inteés simple.
+# Autor: Sergio Díaz González.
+# V 1.0: 19 de septiembre de 2024.
+
+# Valor futuro con interés simple:
+
+valorFinalSimple=function(VA,r,t){
+  xsalida=VA*(1+(r*t))
+  
+  return(xsalida)
+}
