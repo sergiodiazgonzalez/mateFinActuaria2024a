af215f6ff644489250015b86bd129c89cf5b095c
sergiodiazgonzalez <2350461b@umich.mx>
2024-09-20 19:51:46 -0600

    Add files via upload

---
+++ FormulasInteresSimple (1).R
@@ -0,0 +1,38 @@
+# Se presentan las funciones de interés simple
+# Autor: Sergio Día González
+# V 1.0: 20 de septiembre de 2024.
+
+#Valor futuro con interés simple
+
+valorFinalSimple=function(VA,r,t){
+  xSalida=VA*(1+(r*t))
+  
+  return(xSalida)
+}
+
+# Valor actual con interés simple
+
+valorActual=function(VF,r,t){
+  xSalida=VF/(1+(r*t))
+  
+  return(xSalida)
+}
+
+# Interés mensual con interés simple
+
+tasaPeriodo=function(VA,VF,t){
+  xSalida=((VF/VA)-1)/t
+  
+  return(xSalida)
+}
+
+# Tiempo con interés simple
+
+nPeriodos=function(VA,VF,r){
+  xSalida=((VF/VA)-1)/
+  
+  return(xSalida)
+}
+
+
+
